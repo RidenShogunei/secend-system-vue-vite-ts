5f9ee4b86ad752bc574a5351eefb295bbbb30f34
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-03-31 17:43:48 +0800

    改

--- secondsystem/dist/assets/waitPage-uujH0r-t.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,g as o,o as s,a as c,c as n,h as r,s as e,b as _}from"./index-BV_n13TT.js";const l={},d={class:"show"},i=_("br",null,null,-1);function f(u,h){const a=o("a-card");return s(),c("div",d,[n(a,null,{default:r(()=>[e(" 应该在下个版本会更新....."),i,e(" 敬请期待 ")]),_:1})])}const p=t(l,[["render",f]]);export{p as default};
